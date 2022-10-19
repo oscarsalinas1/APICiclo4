1a5f6903d7dbefb6218ce3eb11446c92fdfa2596
oscarsalinas1 <armasali12@yahoo.es>
2022-10-19 10:11:45 -0500

    creacion de Api

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './usuario.controller';
+export * from './aeropuerto.controller';
+export * from './ruta.controller';
+export * from './vuelo.controller';
---
+++ src/models/vuelo.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Vuelo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fecha_inicio: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hora_inicio: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fecha_fin: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hora_fin: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  asientos_vendidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre_piloto: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ruta: string;
+
+
+  constructor(data?: Partial<Vuelo>) {
+    super(data);
+  }
+}
+
+export interface VueloRelations {
+  // describe navigational properties here
+}
+
+export type VueloWithRelations = Vuelo & VueloRelations;
